8cb05e465016df03129f47570cc09ee05568a703
Chompou <tobgiver@gmail.com>
2026-03-23 22:09:34 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "My first flake!";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    lanzaboote.url = "github:nix-community/lanzaboote";
+    lanzaboote.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, lanzaboote, ... }:
+    let 
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      nixosConfigurations = {
+        nixos = lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./configuration.nix 
+            lanzaboote.nixosModules.lanzaboote
+            ./modules/system/secureboot.nix       
+          ];
+        };
+      };
+      homeConfigurations = {
+        chompou = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./home.nix ];
+        };
+      };
+    };
+}
